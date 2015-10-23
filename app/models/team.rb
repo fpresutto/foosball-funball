6a74b33d4f743ecc4d212aa246160f1c1c3cfca3
Fredy Presutto <fredypresutto@gmail.com>
2015-10-23 17:50:03 -0300

    Apply stats in views with games_won and lost by teams, delegating method to user model from team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,12 @@ class Team < ActiveRecord::Base
   has_many :users
   has_many :matches
   has_many :games, :through => :team_games
+
+  def games_won
+    Game.where(winning_team_id: id).count
+  end
+
+  def games_lost
+    Game.where(losing_team_id: id).count
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   #associations
   belongs_to :team
+
+  delegate :games_won, to: :team
+  delegate :games_lost, to: :team
+
 end
