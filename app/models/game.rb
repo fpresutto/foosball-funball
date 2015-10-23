6a771076ea4358594beb1ddf4ff46b4751514e94
Fredy Presutto <fredypresutto@gmail.com>
2015-10-23 17:10:59 -0300

    Apply numericality validation of maximun score

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,8 @@
 class Game < ActiveRecord::Base
   #associations
   belongs_to :match
+  has_many :teams, :through => :team_games
+
+  validates_numericality_of :winning_team_score, only_integer: true, less_than_or_equal_to: 10
+  validates_numericality_of :losing_team_score, only_integer: true, less_than_or_equal_to: 10
 end
