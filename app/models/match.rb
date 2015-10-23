e345398ea59890caf14d9f4f495e4a4acce62ab0
Fredy Presutto <fredypresutto@gmail.com>
2015-10-23 18:49:41 -0300

    Add validations to match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,7 @@ class Match < ActiveRecord::Base
   #associations
   belongs_to :team
   has_many :games
+
+  validates_presence_of :first_game_id, :second_game_id
+  validates_uniqueness_of :first_game_id, :second_game_id
 end
