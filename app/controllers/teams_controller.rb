a1216f0397fc53f4419f20c6ac7fb8d54d0cb7e3
Fredy Presutto <fredypresutto@gmail.com>
2015-10-23 16:55:00 -0300

    Add teams CRUD

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,3 +1,51 @@
 class TeamsController < ApplicationController
+  before_action :find_team, only: [:show]
+
+  def index
+    @teams = Team.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(teams_params)
+
+    if @team.save
+      redirect_to @team
+    else
+      render "New"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @team.update(teams_params)
+      redirect_to @team
+    else
+      render "Edit"
+    end
+  end
+
+  def destroy
+    @team.destroy
+    redirect_to teams_path
+  end
+
+  private
+
+  def teams_params
+    params.require(:team).permit(:name)
+  end
+
+  def find_team
+    @team= Team.find(params[:id])
+  end
 
 end
